2f1cb0a71679b038291e70257179e582f3d08b29
0xkoda <koda_0x@protonmail.com>
2022-02-02 11:16:38 +1000

    add withdraw

--- src/Vault.sol
+++ src/Vault.sol
@@ -1,13 +1,30 @@
-// SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.10;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
-import {ERC20, ERC4626} from "solmate/mixins/ERC4626.sol";
+import {ERC4626} from "solmate/mixins/ERC4626.sol";
+import {RewardsClaimer} from "./RewardsClaimer.sol";
 
-contract vaultERC4626 is ERC4626 {
-    constructor(ERC20 underlying) ERC4626(underlying, "Vault", "V") {}
+abstract contract vaultERC4626 is ERC4626, RewardsClaimer {
+    mapping(address => uint256) balances;
+
+    ERC20 public immutable UNDERLYING;
+
+    constructor(ERC20 underlying) ERC4626(underlying, "Vault", "V") {
+        UNDERLYING = ERC20(underlying);
+    }
 
     function totalAssets() public view override returns (uint256) {
         return asset.balanceOf(address(this));
+    }
+
+    function afterDeposit(uint256 amount) internal override {
+        //logic to distribute funds to pools
+    }
+
+    function beforeWithdraw(uint256 amount) internal override {
+        //logic to distribute funds to pools
+        claimRewards();
+        if (UNDERLYING.balanceOf(address(this)) < amount)
+            _transferAll(UNDERLYING, address(this));
     }
 }
